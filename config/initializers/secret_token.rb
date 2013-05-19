3fc2815d371628bf7c55c873476fb2fc0bdb6ac3
blacklight2000 <vphuvan@yahoo.com>
2013-05-19 05:16:58 -0400

    03 - commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Publishinghackathon::Application.config.secret_token = '1266e99b0e020a02b80b15121500ad2428e68a926198ade7b9bfed0d70495c749ffc723f567b17085c0abb75d2dfeea28fb37525c147d79400b658052ed0ce13'
